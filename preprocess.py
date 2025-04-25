a81e7a878971a2c67d2d7c8e882178b462c94203
anltran <tran.anthony@aol.com>
2025-04-24 20:07:37 -0400

    Map selection complete

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,15 @@
 import pandas as pd
 
-features = ['BORO_NM', 'Longitude', 'Latitude']
+features = ['BORO_NM', 'Longitude', 'Latitude', 'LAW_CAT_CD']
 
 # Load the dataset
 df = pd.read_csv('nypd-complaints.csv', usecols=features)
 
-# Identify rows with any zero
+# Identify rows with any zero and drop them
 rows_with_zeros = (df == 0).any(axis=1)
-
-# Drop rows with any zero
 df = df[~rows_with_zeros]
 
-# Drop rows with any NaN
+# Drop rows with any null values in BORO_NM
 df = df[df.BORO_NM.isin(['MANHATTAN','QUEENS','BROOKLYN','BRONX','STATEN ISLAND'])]
 
 # Export the cleaned DataFrame to a CSV file
